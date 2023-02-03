79231f11f8c4d1c7b6e78815f95f1a6939b3a6c1
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2023-02-03 12:07:51 -0600

    09-synchronized-keyword

--- src/main/java/mx/unam/concurrent/App.java
+++ src/main/java/mx/unam/concurrent/App.java
@@ -2,26 +2,44 @@ package mx.unam.concurrent;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
 public class App {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws InterruptedException {
         Counter c = new Counter();
         c.test();
     }
 }
-
 class Counter {
     int count = 0;
-    void increment() {
+    synchronized void increment() {
         count = count + 1;
     }
-    public void test() {
+    public void test() throws InterruptedException {
         int numProcessors = Runtime.getRuntime().availableProcessors();
-        ExecutorService executor = Executors.newFixedThreadPool(numProcessors);
+        ExecutorService executor = Executors
+            .newFixedThreadPool(numProcessors);
         IntStream.range(0, 10000)
             .forEach(i -> executor.submit(this::increment));
-        executor.shutdown();
+
+        stop(executor);
         System.out.println(count);
+    }
+
+    public static void stop(ExecutorService executor) {
+        try {
+            executor.shutdown();
+            // give it time to finish
+            executor.awaitTermination(60, TimeUnit.SECONDS);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        } finally {
+            if (!executor.isTerminated()) {
+                System.out.println("Termination interrupted");
+            }
+            executor.shutdown();
+        }
     }
 }

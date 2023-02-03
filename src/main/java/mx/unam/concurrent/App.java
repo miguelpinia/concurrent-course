7309a285e2b56282747b16aa3b7a3f53cb02b1ec
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2023-02-03 12:32:21 -0600

    10-lock-reentrantlock

--- src/main/java/mx/unam/concurrent/App.java
+++ src/main/java/mx/unam/concurrent/App.java
@@ -1,45 +1,35 @@
 package mx.unam.concurrent;
 
-import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
-import java.util.stream.IntStream;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class App {
 
     public static void main(String[] args) throws InterruptedException {
-        Counter c = new Counter();
-        c.test();
-    }
-}
-class Counter {
-    int count = 0;
-    synchronized void increment() {
-        count = count + 1;
-    }
-    public void test() throws InterruptedException {
         int numProcessors = Runtime.getRuntime().availableProcessors();
         ExecutorService executor = Executors
             .newFixedThreadPool(numProcessors);
-        IntStream.range(0, 10000)
-            .forEach(i -> executor.submit(this::increment));
-
-        stop(executor);
-        System.out.println(count);
-    }
-
-    public static void stop(ExecutorService executor) {
-        try {
-            executor.shutdown();
-            // give it time to finish
-            executor.awaitTermination(60, TimeUnit.SECONDS);
-        } catch (InterruptedException ex) {
-            ex.printStackTrace();
-        } finally {
-            if (!executor.isTerminated()) {
-                System.out.println("Termination interrupted");
-            }
-            executor.shutdown();
-        }
+        ReentrantLock lock = new ReentrantLock();
+        executor.submit(() -> {
+                lock.lock();
+                try {
+                    TimeUnit.SECONDS.sleep(4);
+                    System.out.println("wake up");
+                } catch (InterruptedException ex) {
+                    ex.printStackTrace();
+                } finally {
+                    lock.unlock();
+                }
+            });
+        executor.submit(() -> {
+                System.out.printf("Locked: %b\n", lock.isLocked());
+                System.out.printf("Held by me: %b\n",
+                                  lock.isHeldByCurrentThread());
+                boolean locked = lock.tryLock();
+                System.out.printf("Lock aquired: %b\n", locked);
+            });
+        executor.shutdown();
     }
 }

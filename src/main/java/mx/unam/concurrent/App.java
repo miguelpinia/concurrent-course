b457865b511e824553dab92881737e40ab044474
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2023-02-03 11:57:28 -0600

    07-batch-execution

--- src/main/java/mx/unam/concurrent/App.java
+++ src/main/java/mx/unam/concurrent/App.java
@@ -1,41 +1,48 @@
 package mx.unam.concurrent;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 public class App {
     public static void main(String args[]) throws ExecutionException {
-        Callable<Integer> task = () -> {
-            try {
-                TimeUnit.SECONDS.sleep(4);
-                return 42;
-            }
-            catch (InterruptedException e) {
-                System.out.println("Error " + e.getMessage());
-                e.printStackTrace();
-            }
-            return -1;
-        };
-
-        ExecutorService executor = Executors.newFixedThreadPool(1);
         try {
-            Future<Integer> future = executor.submit(task);
-            System.out.printf("Future done? %b\n", future.isDone());
-            Integer result = future.get(1, TimeUnit.SECONDS);
-            System.out.printf("Future done? %b\n", future.isDone());
-            System.out.printf("Result: %d\n", result);
+            ExecutorService executor = Executors.newFixedThreadPool(1);
+
+            List<Callable<String>> callables = Arrays
+                .asList(
+                        () -> {
+                            TimeUnit.SECONDS.sleep(2);
+                            return "task 1";
+                        },
+                        () -> {
+                            TimeUnit.SECONDS.sleep(2);
+                            return "task 2";
+                        },
+                        () -> {
+                            TimeUnit.SECONDS.sleep(2);
+                            return "task 3";
+                        });
+
+            executor.invokeAll(callables)
+                .stream()
+                .map(future -> {
+                        try {
+                            return future.get();
+                        } catch (Exception e) {
+                            throw new IllegalStateException(e);
+                        }
+                    })
+                .forEach(System.out::println);
             executor.shutdown();
         }
-        catch (InterruptedException | ExecutionException
-               | TimeoutException e) {
-            System.out.printf("Error %s\n", e.getLocalizedMessage());
+        catch (InterruptedException e) {
+            System.out.println("Error " + e.getMessage());
             e.printStackTrace();
-            executor.shutdown();
         }
     }
 }

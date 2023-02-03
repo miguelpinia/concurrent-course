2a21b45bb387055ca693b2c202635ba30074d5a3
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2023-02-03 11:18:08 -0600

    03-join-thread

--- src/main/java/mx/unam/concurrent/App.java
+++ src/main/java/mx/unam/concurrent/App.java
@@ -2,57 +2,60 @@ package mx.unam.concurrent;
 
 public class App {
 
-    public static void main(String[] args) {
-        MyThread t1 = new MyThread("First Thread");
-        MyThread t2 = new MyThread("Second Thread");
-
-        try {
-            Thread.sleep(500); // Sleeping for 500ms
-            t1.stop();
-            t2.stop();
-            Thread.sleep(500);
-        } catch (InterruptedException e) {
-            System.out.format("Interrupted Exception: %s\n",
-                    e.getMessage());
-            e.printStackTrace();
-        }
-        System.out.println("Exiting the main thread");
-    }
-}
-
-class MyThread implements Runnable {
-
-    private boolean exit;
-    private String name;
-    Thread t;
-
-    public MyThread(String threadName) {
-        name = threadName;
-        t = new Thread(this, name);
-        System.out.format("New Thread: %s\n", t.toString());
-        exit = false;
-        t.start(); // Starting the thread
+    static void threadMessage(String message) {
+        String threadName = Thread.currentThread().getName();
+        System.out.format("%s: %s%n", threadName, message);
     }
 
-    @Override
-    public void run() {
-        int i = 0;
-        while (!exit) {
-            System.out.format("%s: %d\n", name, i);
-            i++;
+    private static class MessageLoop
+        implements Runnable {
+        public void run() {
+            String importantInfo[] = {
+                "Some content",
+                "Another String",
+                "Doing nothing",
+                "I'm close to finishing"
+            };
             try {
-                Thread.sleep(100); // Sleeping for 100ms
-            }
-            catch (InterruptedException e) {
-                System.out.format("Interrupted Exception:  %s\n",
-                                  e.getMessage());
-                e.printStackTrace();
+                for (int i = 0; i < importantInfo.length; i++) {
+                    Thread.sleep(4000);
+                    threadMessage(importantInfo[i]);
+                }
+            } catch (InterruptedException e) {
+                threadMessage("I wasn't done!");
             }
         }
     }
 
-    public void stop() {
-        exit = true;
+    public static void main(String args[])
+        throws InterruptedException {
 
+        long patience = 1000 * 60 * 60;
+        if (args.length > 0) {
+            try {
+                patience = Long.parseLong(args[0]) * 1000;
+            } catch (NumberFormatException e) {
+                System.err.println("Argument must be an integer.");
+                System.exit(1);
+            }
+        }
+
+        threadMessage("Starting MessageLoop thread");
+        long startTime = System.currentTimeMillis();
+        Thread t = new Thread(new MessageLoop());
+        t.start();
+
+        threadMessage("Waiting for MessageLoop thread to finish");
+        while (t.isAlive()) {
+            threadMessage("Still waiting...");
+            t.join(1000);
+            if (((System.currentTimeMillis() - startTime) > patience)
+                && t.isAlive()) {
+                threadMessage("Tired of waiting!");
+                t.interrupt();
+                t.join();
+            }
+        }
+        threadMessage("Finally!");
     }
 }

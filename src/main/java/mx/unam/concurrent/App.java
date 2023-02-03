8b8678b3e8561cc7a523ef55b47d8ece6f4f2ff8
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2023-02-03 12:59:56 -0600

    14-countdownlatch

--- src/main/java/mx/unam/concurrent/App.java
+++ src/main/java/mx/unam/concurrent/App.java
@@ -1,66 +1,114 @@
 package mx.unam.concurrent;
 
-import java.util.concurrent.Executors;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.Semaphore;
 import java.util.stream.IntStream;
 
 public class App {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] argv){
         int numProcessors = Runtime.getRuntime().availableProcessors();
-        ExecutorService executor = Executors.newFixedThreadPool(numProcessors);
-        Semaphore semaphore = new Semaphore(numProcessors / 2);
-
-        Runnable longRunningTask = () -> {
-            boolean permit = false;
-            try {
-                permit = semaphore.tryAcquire(1, TimeUnit.SECONDS);
-                if (permit) {
-                    System.out.println("Semaphore acquired");
-                    sleep(5);
-                } else {
-                    System.out.println("Could not acquire semaphore");
-                }
-            }
-            catch (InterruptedException e) {
-                System.out.println("Error " + e.getMessage());
-                e.printStackTrace();
-            } finally {
-                if (permit) {
-                    semaphore.release();
-                }
-            }
-        };
-
+        ExecutorService consumerExecutors = Executors.newFixedThreadPool(numProcessors);
+        List<Message> queue = Collections.synchronizedList(new LinkedList<Message>());
+        CountDownLatch doneSignal = new CountDownLatch(1);
+        CountDownLatch doneProducingSignal = new CountDownLatch(1);
+        CountDownLatch doneConsumingSignal = new CountDownLatch(numProcessors);
         IntStream.range(0, numProcessors)
-            .forEach(i -> executor.submit(longRunningTask));
+            .forEach(i -> {
+                    String name = String.format("%d", i);
+                    consumerExecutors.execute(new Consumer(name, queue,
+                                                           doneProducingSignal,
+                                                           doneConsumingSignal));
+                });
+        queue.add(new Message( "1", 15000, doneSignal));
+        queue.add(new Message( "2", 15000, new CountDownLatch(1)));
+        doneProducingSignal.countDown();
+        boolean doneProcessing = false;
+        try {
+            doneProcessing = doneSignal.await(3, TimeUnit.SECONDS);
+        } catch (InterruptedException e1) {
+            e1.printStackTrace();
+        }
+        if ( doneProcessing ){
+            System.out.println( "Processing is done.");
+        } else {
+            System.out.println( "Processing is still running.");
+        }
 
-        stop(executor);
-        System.out.println("Finishing program");
+        System.out.println( "Shutting down the consumerExecutors");
+        doneProducingSignal.countDown();
+        try {
+            doneConsumingSignal.await();
+        } catch (InterruptedException e1) {
+            e1.printStackTrace();
+        }
+        consumerExecutors.shutdown();
+        System.out.println( "Done");
+    }
+}
+
+class Consumer implements Runnable {
+    private String id;
+    private List<Message> queue;
+    private CountDownLatch doneProducing;
+    private CountDownLatch doneConsuming;
+
+    Consumer(String id, List<Message> queue,
+             CountDownLatch doneProducing,
+             CountDownLatch doneConsuming){
+        this.id = id;
+        this.queue = queue;
+        this.doneProducing = doneProducing;
+        this.doneConsuming = doneConsuming;
     }
 
-    public static void stop(ExecutorService executor) {
-        try {
-            executor.shutdown();
-            // give it time to finish
-            executor.awaitTermination(60, TimeUnit.SECONDS);
-        } catch (InterruptedException ex) {
-            ex.printStackTrace();
-        } finally {
-            if (!executor.isTerminated()) {
-                System.out.println("Termination interrupted");
+    @Override
+    public void run() {
+        while(doneProducing.getCount() != 0 || !queue.isEmpty()){
+            Message m = null;
+            synchronized(queue){
+                if(!queue.isEmpty()) m = queue.remove(0);
             }
-            executor.shutdown();
+            if(m != null) consume(m);
         }
+        System.out.printf("Consumer %s done\n", id);
+        doneConsuming.countDown();
     }
-
-    public static void sleep(int seconds) {
+    public void consume(Message m ){
+        System.out.printf("Consumer %s consuming message %s\n",
+                          id, m.getId());
         try {
-            TimeUnit.SECONDS.sleep(seconds);
+            Thread.sleep(m.getTime());
         } catch (InterruptedException e) {
-            throw new IllegalStateException(e);
+            e.printStackTrace();
         }
+        System.out.printf("Consumer %s done consumming msssage %s\n",
+                          id, m.getId());
+        m.getLatch().countDown();
+    }
+}
+
+class Message {
+    private String id;
+    private int time;
+    private CountDownLatch latch;
+    Message(String id, int time, CountDownLatch latch){
+        this.id = id;
+        this.time = time;
+        this.latch = latch;
+    }
+    public String getId() {
+        return id;
+    }
+    public int getTime() {
+        return time;
+    }
+    public CountDownLatch getLatch() {
+        return latch;
     }
 }

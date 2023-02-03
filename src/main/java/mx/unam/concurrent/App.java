ce12d233f1ce426376e975eade5e5de6afb8b079
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2023-02-03 12:44:13 -0600

    13-semaphores

--- src/main/java/mx/unam/concurrent/App.java
+++ src/main/java/mx/unam/concurrent/App.java
@@ -3,10 +3,43 @@ package mx.unam.concurrent;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.Semaphore;
 import java.util.stream.IntStream;
 
 public class App {
+
+    public static void main(String[] args) throws InterruptedException {
+        int numProcessors = Runtime.getRuntime().availableProcessors();
+        ExecutorService executor = Executors.newFixedThreadPool(numProcessors);
+        Semaphore semaphore = new Semaphore(numProcessors / 2);
+
+        Runnable longRunningTask = () -> {
+            boolean permit = false;
+            try {
+                permit = semaphore.tryAcquire(1, TimeUnit.SECONDS);
+                if (permit) {
+                    System.out.println("Semaphore acquired");
+                    sleep(5);
+                } else {
+                    System.out.println("Could not acquire semaphore");
+                }
+            }
+            catch (InterruptedException e) {
+                System.out.println("Error " + e.getMessage());
+                e.printStackTrace();
+            } finally {
+                if (permit) {
+                    semaphore.release();
+                }
+            }
+        };
+
+        IntStream.range(0, numProcessors)
+            .forEach(i -> executor.submit(longRunningTask));
+
+        stop(executor);
+        System.out.println("Finishing program");
+    }
 
     public static void stop(ExecutorService executor) {
         try {
@@ -23,16 +56,11 @@ public class App {
         }
     }
 
-    public static void main(String[] args) throws InterruptedException {
-        int numProcessors = Runtime.getRuntime().availableProcessors();
-        ExecutorService executor = Executors
-            .newFixedThreadPool(numProcessors);
-        AtomicInteger atomicInt = new AtomicInteger(0);
-
-        IntStream.range(0, 10000)
-            .forEach(i -> executor.submit(atomicInt::incrementAndGet));
-
-        stop(executor);
-        System.out.println("Value is: " + atomicInt.get());
+    public static void sleep(int seconds) {
+        try {
+            TimeUnit.SECONDS.sleep(seconds);
+        } catch (InterruptedException e) {
+            throw new IllegalStateException(e);
+        }
     }
 }

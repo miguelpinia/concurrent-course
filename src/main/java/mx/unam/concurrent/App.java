96f35bcf33ab8e81a9cfa4fe21d8183eb7cb157f
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2023-02-03 12:41:04 -0600

    12-atomicinteger

--- src/main/java/mx/unam/concurrent/App.java
+++ src/main/java/mx/unam/concurrent/App.java
@@ -3,50 +3,36 @@ package mx.unam.concurrent;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.ReentrantLock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
-import java.util.concurrent.locks.ReadWriteLock;
-import java.util.Map;
-import java.util.HashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
 
 public class App {
 
+    public static void stop(ExecutorService executor) {
+        try {
+            executor.shutdown();
+            // give it time to finish
+            executor.awaitTermination(60, TimeUnit.SECONDS);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        } finally {
+            if (!executor.isTerminated()) {
+                System.out.println("Termination interrupted");
+            }
+            executor.shutdown();
+        }
+    }
+
     public static void main(String[] args) throws InterruptedException {
         int numProcessors = Runtime.getRuntime().availableProcessors();
-        ExecutorService executor = Executors.
-            newFixedThreadPool(numProcessors);
-        Map<String, String> map = new HashMap<>();
-        ReadWriteLock lock = new ReentrantReadWriteLock();
-        executor.submit(() -> {
-                lock.writeLock().lock();
-                try {
-                    System.out.println("Putting information into the map");
-                    TimeUnit.SECONDS.sleep(4);
-                    map.put("foo", "bar");
-                } catch (InterruptedException ex) {
-                    ex.printStackTrace();
-                } finally {
-                    lock.writeLock().unlock();
-                }
-            });
+        ExecutorService executor = Executors
+            .newFixedThreadPool(numProcessors);
+        AtomicInteger atomicInt = new AtomicInteger(0);
 
-        Runnable readTask = () -> {
-            lock.readLock().lock();
-            try {
-                String threadName = Thread.currentThread().getName();
-                System.out.printf("Name %s, value: %s\n",
-                                  threadName, map.get("foo"));
-                TimeUnit.SECONDS.sleep(1);
-            } catch(InterruptedException ex) {
-                ex.printStackTrace();
-            } finally {
-                lock.readLock().unlock();
-            }
-        };
+        IntStream.range(0, 10000)
+            .forEach(i -> executor.submit(atomicInt::incrementAndGet));
 
-        executor.submit(readTask);
-        executor.submit(readTask);
-
-        executor.shutdown();
+        stop(executor);
+        System.out.println("Value is: " + atomicInt.get());
     }
 }

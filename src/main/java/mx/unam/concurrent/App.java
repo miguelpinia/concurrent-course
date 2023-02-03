95cfee7f22c057e031f5f2476f3c72dfb45b9ba0
Miguel Angel Piña Avelino <miguel_pinia@ciencias.unam.mx>
2023-02-03 11:24:28 -0600

    05-callables-and-futures

--- src/main/java/mx/unam/concurrent/App.java
+++ src/main/java/mx/unam/concurrent/App.java
@@ -1,31 +1,39 @@
 package mx.unam.concurrent;
 
+import java.util.concurrent.Callable;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 public class App {
+    public static void main(String args[]) throws ExecutionException {
+        Callable<Integer> task = () -> {
+            try {
+                TimeUnit.SECONDS.sleep(2);
+                return 42;
+            }
+            catch (InterruptedException e) {
+                System.out.println("Error " + e.getMessage());
+                e.printStackTrace();
+            }
+            return -1;
+        };
 
-    private static void doLongWork(String name) {
-        String message = String.format("Hello %s, how is going?", name);
-        System.out.println(message);
+        ExecutorService executor = Executors.newFixedThreadPool(1);
         try {
-            Thread.sleep(1001);
+            Future<Integer> future = executor.submit(task);
+            System.out.printf("Future done? %b\n", future.isDone());
+            Integer result = future.get();
+            System.out.printf("Future done? %b\n", future.isDone());
+            System.out.printf("Result: %d\n", result);
+            executor.shutdown();
         }
-        catch (InterruptedException e) {
-            System.out.println("Error " + e.getMessage());
+        catch (InterruptedException | ExecutionException e) {
+            System.out.printf("Error %s\n", e.getMessage());
             e.printStackTrace();
+            executor.shutdown();
         }
-    }
-
-    public static void main(String[] args) {
-        int numProcessors = Runtime.getRuntime().availableProcessors();
-        ExecutorService executor = Executors
-            .newFixedThreadPool(numProcessors);
-        for (int i = 0; i < numProcessors; i++) {
-            final int name = i;
-            executor.execute(() -> doLongWork(String.format("thread %d",
-                                                            name)));
-        }
-        executor.shutdown();
     }
 }
